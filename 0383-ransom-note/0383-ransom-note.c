6c208432e3ad5731279e84d90fcb0b90d9af0274
Sabih Qadri <sabihqadri2003@gmail.com>
2026-08-01 16:04:42 -0700

    Time: 0 ms (100%), Space: 9.5 MB (72.06%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.c
@@ -0,0 +1,19 @@
+bool canConstruct(char* ransomNote, char* magazine) {
+    int count[26] = {0};
+
+    // Count letters in magazine
+    for (int i = 0; magazine[i] != '\0'; i++) {
+        count[magazine[i] - 'a']++;
+    }
+
+    // Use letters for ransom note
+    for (int i = 0; ransomNote[i] != '\0'; i++) {
+        count[ransomNote[i] - 'a']--;
+
+        if (count[ransomNote[i] - 'a'] < 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
