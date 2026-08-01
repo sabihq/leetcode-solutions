e3301d5ce146ef4dd61bbcf61f1db67fbd5afbf7
Sabih Qadri <sabihqadri2003@gmail.com>
2026-08-01 15:36:30 -0700

    Time: 3 ms (25.94%), Space: 10.5 MB (57.7%) - LeetHub

---
+++ 0135-candy/0135-candy.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+int candy(int* ratings, int ratingsSize) {
+    int* candies = malloc(ratingsSize * sizeof(int));
+
+    // Every child gets at least one candy.
+    for (int i = 0; i < ratingsSize; i++) {
+        candies[i] = 1;
+    }
+
+    // Left to right: compare each child with the left neighbor.
+    for (int i = 1; i < ratingsSize; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            candies[i] = candies[i - 1] + 1;
+        }
+    }
+
+    // Right to left: compare each child with the right neighbor.
+    for (int i = ratingsSize - 2; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1] &&
+            candies[i] <= candies[i + 1]) {
+            candies[i] = candies[i + 1] + 1;
+        }
+    }
+
+    int total = 0;
+
+    for (int i = 0; i < ratingsSize; i++) {
+        total += candies[i];
+    }
+
+    free(candies);
+    return total;
+}
