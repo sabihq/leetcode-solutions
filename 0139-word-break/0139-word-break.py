15703dab9eb44935dd58ded80f0afbc7783decee
Sabih Qadri <sabihqadri2003@gmail.com>
2026-09-08 00:52:33 -0700

    Time: 2 ms (90.89%), Space: 12.3 MB (75.32%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        words = set(wordDict)
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+
+        for end in range(1, len(s) + 1):
+            for start in range(end):
+                if dp[start] and s[start:end] in words:
+                    dp[end] = True
+                    break
+
+        return dp[len(s)]
